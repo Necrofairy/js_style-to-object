7a0708ef40d8c03a94c195ab80661e81e14f887e
Ivan Berkut <necrofairywork@gmail.com>
2025-07-01 16:26:15 +0300

    Solution

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -1,30 +1,67 @@
 'use strict';
 
+const complexStylesString = `
+  background-color:      #fff;
+-webkit-border-radius: 5px;
+  border-radius     : 5px;
+  border: 1px solid #e8e8e8;
+  -webkit-box-sizing: border-box;
+  box-sizing: border-box;
+  clear   : both       ;
+  cursor: pointer;
+  float: left;
+  font-family: inherit;
+      font-size: 14px;
+  font-weight: 400;
+  height: 42px;
+  line-height:    40px;
+  outline: 0;
+  padding-left    : 18px;
+  padding-right: 30px;
+  ;
+
+  ;
+  position: relative;
+
+
+  text-align: left !important;
+  -webkit-transition: all .2s ease-in-out;
+  transition: all .2s ease-in-out;
+  -moz-user-select: none;
+  -ms-user-select: none;
+  user-select: none;
+
+
+  white-space: nowrap;
+  width: auto;
+`;
+
 /**
  * @param {string} sourceString
  *
  * @return {object}
  */
 function convertToObject(sourceString) {
-  const arrayStylesWithoutSemicolones = sourceString
-    .replaceAll(';', ':')
-    .split(':');
-  const arrayTrim = arrayStylesWithoutSemicolones.map((x) => x.trim());
-  const arrayFilter = arrayTrim.filter((x) => x !== '');
-  const arrayKeys = arrayFilter.filter((x, i) => i % 2 === 0);
-  const arrayValues = arrayFilter.filter((x, i) => i % 2 !== 0);
+  const arraySplit = sourceString.split(';');
+  const arrayMap = arraySplit.map((string) => [
+    string.slice(0, string.indexOf(':')).trim(),
+    string.slice(string.indexOf(':') + 1).trim(),
+  ]);
+  const arrayFilter = arrayMap.filter((array) => array[0] !== '');
 
-  return createObject(arrayKeys, arrayValues);
+  return createObject(arrayFilter);
 }
 
-function createObject(keys, values) {
+function createObject(array) {
   const result = {};
 
-  for (let i = 0; i < keys.length; i++) {
-    result[keys[i]] = values[i];
+  for (const insideArray of array) {
+    result[insideArray[0]] = insideArray[1];
   }
 
   return result;
 }
+
+convertToObject(complexStylesString);
 
 module.exports = convertToObject;
